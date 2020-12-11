d6c09d279391b21420ce783f25d8c3cc86c441a0
Lucia <lucygm.dev@gmail.com>
2020-12-11 23:57:46 +0100

    Mock-Up headboard

--- includes/header.php
+++ includes/header.php
@@ -2,16 +2,17 @@
 <header>
     <!--Logo de la pagina-->
     <div id="cabecera">
-        <h1><a href="../index.php">LucyGM_DEV Academy</a></h1>
+        <h1><a href="/index.php">LucyGM_DEV Academy</a></h1>
     </div>
     <!--Menu-->
     <nav id="menu">
         <ul>
-            <li><a href="#">HTML</li>
+            <li><a href="#">HTML</a></li>
             <li><a href="#">CSS</a></li>
             <li><a href="#">JavaScript</a></li>
             <li><a href="#">PHP</a></li>
             <li><a href="#">DataBases</a></li>
+            <li><a href="#">About Me</a></li>
         </ul>
     </nav>
 </header>
